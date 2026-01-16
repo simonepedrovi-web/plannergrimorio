07b1a49ef157b4ab485461f12b742e3764e2527b
bruxa <simonepedrovi@gmail.com>
2026-01-16 19:50:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,18 @@ self.addEventListener('activate', () => {
 
 self.addEventListener('notificationclick', event => {
   event.notification.close();
+
   event.waitUntil(
-    self.clients.openWindow('./')
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(clientList => {
+      for (const client of clientList) {
+        if (client.url.includes('index.html') && 'focus' in client) {
+          return client.focus();
+        }
+      }
+
+      if (clients.openWindow) {
+        return clients.openWindow('/index.html');
+      }
+    })
   );
 });
